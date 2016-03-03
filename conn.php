de56aa061fcb7c57e11f13aea5e6e659d76d9517
Dylan Foster <fdylan52@yahoo.com>
2016-03-03 17:29:17 -0500

    Update
    changed server name to heroku manhuntserverapp

--- conn.php
+++ conn.php
@@ -2,7 +2,7 @@
 $db_name = "manhunt";
 $mysql_username = "root";
 $mysql_password = "";
-$server_name = "localhost";
+$server_name = "manhuntserverapp";
 $conn = mysqli_connect($server_name, $mysql_username, $mysql_password, $db_name);
 echo "Connection Successful"
 ?>
