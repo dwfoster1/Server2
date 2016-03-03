e6f6cda3f093807157fd77c766e78677143022bf
Dylan Foster <fdylan52@yahoo.com>
2016-03-02 22:13:40 -0500

    Add files via upload
    PHP files necessary for server

---
+++ register.php
@@ -0,0 +1,17 @@
+<?php
+require "conn.php";
+#$name = $_POST["name"];
+#$username = $_POST["username"];
+#$password = $_POST["password"];
+$name = "Dylan";
+$username = "dwfoster";
+$password = "capstone";
+
+$mysql_query = "insert into user_info (name, username, password) values ('$name','$username','$password')";
+    if($conn->query($mysql_qry) === TRUE){
+        echo "DB Record Insert Successful!!!";
+    }else{
+        echo "DB Record Insert Error: ".$mysql_qry."<br>".$conn->error;
+    }
+$conn->close();
+?>
