a27fa28dd1b5aa8e1dd9c2e16b74c62a972d42a2
Dylan Foster <fdylan52@yahoo.com>
2016-03-06 21:14:08 -0500

    Update conn.php

--- conn.php
+++ conn.php
@@ -1,8 +1,25 @@
 <?php
-$db_name = "manhunt";
-$mysql_username = "root";
-$mysql_password = "";
-$server_name = "manhuntserverapp";
-$conn = mysqli_connect($server_name, $mysql_username, $mysql_password, $db_name);
-echo "Connection Successful"
+/**
+*conn.php - script to establish the database connection.
+*
+*@author Dylan Foster
+*@version February 2016
+*/
+/**The database URL*/
+$url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+
+/**The server URL*/
+$server = $url["host"];
+
+/**The database username*/
+$username = $url["user"];
+
+/**The database password*/
+$password = $url["pass"];
+
+/**The database name*/
+$db = substr($url["path"], 1);
+
+/**Makes the connection to the database*/
+$conn = new mysqli($server, $username, $password, $db);
 ?>
