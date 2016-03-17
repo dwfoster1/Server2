b49a872f05b177e4e39606ad6a47514b433c19c4
Dylan Foster <fdylan52@yahoo.com>
2016-03-17 14:00:04 -0400

    changed name so conn and register names don't fight

--- register.php
+++ register.php
@@ -1,16 +1,16 @@
 <?php
 require "conn.php";
-$name = $_POST["name"];
-$username = $_POST["user"];
-$password = $_POST["pass"];
+$name_db = $_POST["name"];
+$username_db = $_POST["user"];
+$password_db = $_POST["pass"];
 
-$mysql_query = "INSERT into user_info (name, username, password) values ('$name','$username','$password')";
+$mysql_query = "INSERT into user_info (name, username, password) values ('$name_db','$username_db','$password_db')";
     if($conn->query($mysql_qry) === TRUE){
         echo "DB Record Insert Successful!!!";
     }else{
         echo "DB Record Insert Error: ".$mysql_qry."<br>".$conn->error;
-		echo $username
-		echo $password
+		echo $username_db
+		echo $password_db
     }
 $conn->close();
 ?>
