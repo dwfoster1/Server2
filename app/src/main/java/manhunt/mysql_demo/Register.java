75629a3e78cc77d25119d0241ec7f330fb104980
Dylan <dwfoster1@catamount.wcu.edu>
2016-03-18 01:53:58 -0400

    updated for shared preferences and lobby

--- app/src/main/java/manhunt/mysql_demo/Register.java
+++ app/src/main/java/manhunt/mysql_demo/Register.java
@@ -2,28 +2,26 @@ package manhunt.mysql_demo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.widget.EditText;
 
 public class Register extends AppCompatActivity {
-    EditText name, username, password;
+    EditText name_db, username_db, password_db;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
 
-        name = (EditText)findViewById(R.id.et_name);
-        username = (EditText)findViewById(R.id.et_username);
-        password = (EditText)findViewById(R.id.et_password);
+        name_db = (EditText)findViewById(R.id.et_name);
+        username_db = (EditText)findViewById(R.id.et_username);
+        password_db = (EditText)findViewById(R.id.et_password);
     }
 
     public void OnReg(View view) {
-        String str_name = name.getText().toString();
-        String str_username = username.getText().toString();
-        String str_password = password.getText().toString();
+        String str_name = name_db.getText().toString();
+        String str_username = username_db.getText().toString();
+        String str_password = password_db.getText().toString();
         String type = "register";
         BackgroundWorker backgroundWorker = new BackgroundWorker(this);
         backgroundWorker.execute(type, str_name, str_username, str_password);
